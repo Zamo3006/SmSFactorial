d8228278cbb69870e2f3acec53170a98d2eb4aee
Budig <1>
2018-04-03 19:35:46 +0200

    added timer

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 #define SMALL uint32_t
 #define BIG uint64_t
 #define SMALL_LENGTH 31
-#define ARRAY_LENGTH 49000
+#define ARRAY_LENGTH 50000
 
 int main() {
     SMALL i = 100000;
@@ -12,6 +13,8 @@ int main() {
     BIG full = pow(2, SMALL_LENGTH) - 1;
     BIG rest = 0;
     BIG results[ARRAY_LENGTH] = {1};
+    clock_t t;
+    t = clock();
     for (BIG mult = 1; mult <= i; mult++) {
         for (SMALL current = 0; current <= counter; current++) {
             results[current] *= mult;
@@ -23,23 +26,35 @@ int main() {
             }
         }
     }
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
 
-    printf("Number of entries: %d,", counter+1);
+    FILE *f = fopen("result.txt", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        return(1);
+    }
 
+    printf("Factorial took %f seconds to execute \n", time_taken);
+    printf("Number of entries: %d,", ++counter);
     printf("\nResults: ");
-    printf("%u \n", results[0]);
+    fprintf(f, "Factorial took %f seconds to execute \n", time_taken);
+    fprintf(f, "Number of entries: %d,", counter);
+    fprintf(f, "\nResults: ");
     BIG printBit;
     BIG andBits;
-    do  {
-        for (SMALL x = SMALL_LENGTH ; x > 0; x--) {
-            printBit = results[counter] >> x;
-            andBits = pow(2,x);
-            results[counter] &= ~andBits;
-         //   printf("%u", printBit);
-        }
-        if(counter != 0)
+    do{
         --counter;
-    }while(counter > 0);
+        for (SMALL x = SMALL_LENGTH ; x > 0; x--) {
+            SMALL xx = x-1;
+            printBit = results[counter] >> xx;
+            andBits = pow(2,xx);
+            results[counter] &= ~andBits;
+            fprintf(f, "%u", printBit);
+        }
 
+    }while(counter > 0);
+    fclose(f);
     return 0;
 }
