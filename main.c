651d0a489ad52222a21d500ee4d594a61e19b3b6
Budig <2>
2018-04-04 09:35:10 +0200

    added temp

--- main.c
+++ main.c
@@ -13,27 +13,31 @@ int main() {
     BIG full = pow(2, SMALL_LENGTH) - 1;
     BIG rest = 0;
     BIG results[ARRAY_LENGTH] = {1};
+    BIG temp;
     clock_t t;
     t = clock();
-    for (BIG mult = 1; mult <= i; mult++) {
+    for (SMALL mult = 1; mult <= i; mult++) {
         for (SMALL current = 0; current <= counter; current++) {
-            results[current] *= mult;
-            results[current] += rest;
-            rest = results[current] >> SMALL_LENGTH;
-            results[current] = results[current] & full;
+            temp = results[current] * mult;
+            temp += rest;
+            rest = temp >> SMALL_LENGTH;
+            results[current] = temp & full;
+            //results[current] *= mult;
+            //results[current] += rest;
+            //rest = results[current] >> SMALL_LENGTH;
+            //results[current] = results[current] & full;
             if (rest != 0 && current == counter) {
                 ++counter;
             }
         }
     }
     t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    double time_taken = ((double) t) / CLOCKS_PER_SEC;
 
     FILE *f = fopen("result.txt", "w");
-    if (f == NULL)
-    {
+    if (f == NULL) {
         printf("Error opening file!\n");
-        return(1);
+        return (1);
     }
 
     printf("Factorial took %f seconds to execute \n", time_taken);
@@ -44,17 +48,17 @@ int main() {
     fprintf(f, "\nResults: ");
     BIG printBit;
     BIG andBits;
-    do{
+    do {
         --counter;
-        for (SMALL x = SMALL_LENGTH ; x > 0; x--) {
-            SMALL xx = x-1;
+        for (SMALL x = SMALL_LENGTH; x > 0; x--) {
+            SMALL xx = x - 1;
             printBit = results[counter] >> xx;
-            andBits = pow(2,xx);
+            andBits = pow(2, xx);
             results[counter] &= ~andBits;
             fprintf(f, "%u", printBit);
         }
 
-    }while(counter > 0);
+    } while (counter > 0);
     fclose(f);
     return 0;
 }
